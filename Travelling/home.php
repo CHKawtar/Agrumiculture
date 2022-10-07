5df186f2740ddb8e9f93f06ee747eb44b898830e
kawtar Chouiref <93385912+CHKawtar@users.noreply.github.com>
2022-10-07 21:05:53 +0200

    Add files via upload

---
+++ Travelling/home.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">AgExpert.</a>
+
+   <nav class="navbar">
+      <a href="home.php">Accueil</a>
+      <a href="about.php">A propos</a>
+      <a href="package.php">package</a>
+      <a href="book.php">Info</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home">
+
+   <div class="swiper home-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide" style="background:url(images/head1.jpg) no-repeat">
+            <div class="content">
+               <span>Explorez, Decouvrir, Deguster</span>
+               <h3>Agrumes, Slice of life</h3>
+               <a href="package.php" class="btn">decouvrir plus</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/head2.jpg) no-repeat">
+            <div class="content">
+               <span>Explorez, Decouvrir, Deguster</span>
+               <h3>Les rimes des agrumes</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/head3.jpg) no-repeat">
+            <div class="content">
+               <span>Explorez, Decouvrir, Deguster</span>
+               <h3>Citron, orange, pamplemousse!</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+         
+      </div>
+
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+
+   </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- services section starts  -->
+
+<section class="services">
+
+   <h1 class="heading-title">Nos  services </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/icon-1.png" alt="">
+         <h3>Endroits</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-2.png" alt="">
+         <h3>tour guide</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-3.png" alt="">
+         <h3>Renseignement</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-4.png" alt="">
+         <h3>Barrieres</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-5.png" alt="">
+         <h3>off road</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-6.png" alt="">
+         <h3>Objectifs</h3>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- services section ends -->
+
+<!-- home about section starts  -->
+
+<section class="home-about">
+
+   <div class="image">
+      <img src="images/unique.jpg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>A propos de nous</h3>
+      <p>L’agriculture est un enjeu social, économique, écologique et sanitaire. Sa bonne gestion et son développement permettent de lutter contre l’insécurité alimentaire et nutritionnelle, accrue par le changement climatique et l’instabilité politique. Avec 40 % de la population travaillant dans le secteur agricole, c’est aussi le principal employeur au niveau mondial.
+AgExpert agit sur les filières agricoles, à toutes les échelles, pour promouvoir un développement rural inclusif et durable et assurer la sécurité alimentaire des populations.</p>
+      <a href="about.php" class="btn">Lire plus</a>
+   </div>
+
+</section>
+
+<!-- home about section ends -->
+
+<!-- home packages section starts  -->
+
+<section class="home-packages">
+
+   <h1 class="heading-title">Nos packages </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/imga11.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Agrumes & tour</h3>
+            <p>La Bergamote est le fruit du Bergamotier (Citrus bergamia). On ne doit ni le confondre avec la Poire Bergamote, ni avec d’autres espèces de différents agrumes apparentés.</p>
+            <a href="book.php" class="btn">Servez nous</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/imga22.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Agrumes & tour</h3>
+            <p>Le Bigaradier, qu’on appelle aussi Orange Amère ou Orange de Séville, pousse sur le Bigaradier (Citrus aurantium). Et il a de nombreuses utilisations.</p>
+            <a href="book.php" class="btn">Servez nous</a>
+         </div>
+      </div>
+      
+      <div class="box">
+         <div class="image">
+            <img src="images/imga33.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Agrumes & tour</h3>
+            <p>Le Calamondin porte de nombreux noms. Par exemple, selon les régions, on va l’appeler oranger d’appartement ou kalamansi entre autres.</p>
+            <a href="book.php" class="btn">Servez nous</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"> <a href="package.php" class="btn">Telechargez plus</a> </div>
+
+</section>
+
+<!-- home packages section ends -->
+
+<!-- home offer section starts  -->
+
+<section class="home-offer">
+   <div class="content">
+      <h3>Le revers de la medaille</h3>
+      <p>Pour faire la différence rapidement entre Clémentine, Mandarine et Orange, la Clémentine, c’est le plus petit des 3.</p>
+      <a href="book.php" class="btn">Servez nous</a>
+   </div>
+</section>
+
+<!-- home offer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> shaikhanas@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> mumbai, india - 400104 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>mr. web designer</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ Travelling/package.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>package</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">AgExpert.</a>
+
+   <nav class="navbar">
+      <a href="home.php">Accueil</a>
+      <a href="about.php">A propos</a>
+      <a href="package.php">package</a>
+      <a href="book.php">Livre</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/header-bg-2.png) no-repeat">
+   <h1>packages</h1>
+</div>
+
+<!-- packages section starts  -->
+
+<section class="packages">
+
+   <h1 class="heading-title">top Agrumes</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/imga11.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Agrumes & tour</h3>
+            <p>La Bergamote est le fruit du Bergamotier (Citrus bergamia). On ne doit ni le confondre avec la Poire Bergamote, ni avec d’autres espèces de différents agrumes apparentés.</p>
+            
+            <a href="book.php" class="btn">Servez nous</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/imga22.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Agrumes & tour</h3>
+            <p>Le Calamondin porte de nombreux noms. Par exemple, selon les régions, on va l’appeler oranger d’appartement ou kalamansi entre autres.</p>
+            <a href="book.php" class="btn">Servez nous</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/imga33.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Agrumes & tour</h3>
+            <p>Le Bigaradier, qu’on appelle aussi Orange Amère ou Orange de Séville, pousse sur le Bigaradier (Citrus aurantium). Et il a de nombreuses utilisations.
+            
+            </p>
+            <a href="book.php" class="btn">Servez nous</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/imga44.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Agrumes & tour</h3>
+            <p>Le Citron fait partie des fruits préférés des Français. Ce bel agrume à l’écorce jaune et à la pulpe très acide, mais au parfum délicieux agrémente de nombreuses recettes dans de nombreuses cuisines </p>
+            <a href="book.php" class="btn">Servez nous</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/imga55.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Agrumes & tour</h3>
+            <p>Le Citron Caviar représente en réalité plusieurs variétés d’agrumes. Voire plusieurs espèces. Puisque finalement, le Citron Caviar pousse à partir de Microcitrus australasica, ou Citrus australisica.</p>
+            <a href="book.php" class="btn">Servez nous</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/imga66.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Agrumes & tour</h3>
+            <p>Le Citron Meyer résulte probablement du croisement entre le Citronnier (Citrus limon) et de l’Oranger Doux (Citrus sinensis).
+            On le trouve souvent comme arbuste d’ornement en pot.
+            </p>
+            <a href="book.php" class="btn">Servez nous</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/imga77.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Agrumes & tour</h3>
+            <p>Le Chinotto est un petit agrume peu connu, mais qui ressemble à une petite orange, un peu plus verdâtre.
+
+En réalité, le Chinotto est une variété de Bigaradier (Citrus aurantium var. myrtifolia). On l’appelle aussi Chinette ou Petit Chinois.
+
+Son goût est loin d’être incroyable. En effet, sa pulpe, assez peu juteuse, reste très amère.</p>
+            <a href="book.php" class="btn">Servez nous</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/imga88.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Agrumes & tour</h3>
+            <p>Pour faire la différence rapidement entre Clémentine, Mandarine et Orange, la Clémentine, c’est le plus petit des 3.
+
+Fruit du Clémentinier (Citrus × clementina), la Clémentine développe un goût sucré et légèrement acidulé, le tout, avec la peau qui s’enlève facilement.
+
+Le Clémentinier est l’hybride entre le Mandarinier et l’Oranger.
+
+            
+            </p>
+            <a href="book.php" class="btn">Servez nous</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/imga99.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Agrumes & tour</h3>
+            <p>La Clemenvilla (clémenvilla), qu’on appelle parfois Nova est une cousine de la Clémentine.
+
+D’ailleurs, le Clémenvillier résulte de l’hybridation entre le Tangelo et le Clémentinier. On le présente souvent comme une variété de Mandarine, mais ce n’en est pas une.
+
+Souvent ovoïde, le fruit du Clémenvillier est un peu plus grand qu’une Mandarine. </p>
+            <a href="book.php" class="btn">Servez nous</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/imga1010.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Agrumes & tour</h3>
+            <p>Le Combava est à la fois un fruit, un condiment et un arbre. Le Citron Combava pousse sur le Combava (Citrus hystrix). Et on utilise presque toutes les parties de la plante.</p>
+            <a href="book.php" class="btn">Servez vous</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/imga1111.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Agrumes & tour</h3>
+            <p>Le Dekopon fait partie des agrumes Japonais. Il a aussi la particularité de ne pas avoir de pépins. Sa peau s’enlève facilement et son goût est goût.
+
+</p>
+            <a href="book.php" class="btn">Servez nous</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/imga1212.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Agrumes & tour</h3>
+            <p>Dans les différents agrumes qu’on trouve au Japon, le Iyokan fait sans doute des plus produits par le pays.
+
+En dehors des îles japonaises, il reste très peu connu.</p>
+            <a href="book.php" class="btn">Servez nous</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"><span class="btn">Telechargez plus</span></div>
+
+</section>
+
+<!-- packages section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> shaikhanas@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> mumbai, india - 400104 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>mr. web designer</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
